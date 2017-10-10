5530a6ff9f9e53f647cb500502f0d646d9f95f69
David Reep <davereep@gmail.com>
2017-10-10 23:05:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeliLine = []
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push[name];
   let num = katzDeliLine.length;
-  return `Welcome ${name}.  You are currently number ${num} in line.`
+  return `Welcome, ${name}.  You are number ${num} in line.`
 }
